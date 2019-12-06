07da12df2717f83421ee1edb895052b153762fde
Mehmet Can Yüney <mehmetyuney@marun.edu.tr>
2019-12-06 20:50:07 +0300

    Halfway commit

---
+++ src/search_algorithm.py
@@ -0,0 +1,10 @@
+class SearchAlgorithm():
+    solution_path = []
+    expanded = []
+    cost = 0
+
+    # going to change depending on the algorithm
+    def search(self):
+        pass
+
+    def
